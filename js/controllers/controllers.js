3780d0b4215d15e22d06b912f38a51dafb6181b6
JC <alfarol@correo.ugr.es>
2015-03-20 19:05:34 +0100

    Prueba web

---
+++ js/controllers/controllers.js
@@ -0,0 +1,6 @@
+angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('DemoCtrl', function ($scope, $log) {
+
+    $scope.user = "Hola mundo";
+
+});
