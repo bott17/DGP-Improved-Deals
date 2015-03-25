179f01a696780bfcbec15836aa2e24b0cb37983c
JC <alfarol@correo.ugr.es>
2015-03-25 17:18:14 +0100

    añadidos nuevos archivos para la prueba

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -3,4 +3,31 @@ angular.module('ui.bootstrap.demo').controller('DemoCtrl', function ($scope, $lo
 
     $scope.user = "Hola mundo";
 
+    $scope.list = 
+    {
+      item1: {
+      	title: 'Heading 1',
+      	body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.'
+      },
+
+      item2: {
+      	title: 'Heading 2',
+      	body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.'
+      },
+
+      item3: {
+      	title: 'Heading 3',
+      	body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.'
+      },
+
+    };
+
+    console.log($scope.list);
+
+    $scope.mostrar = true;
+
+    $scope.toggleMode = function() {
+		$scope.mostrar = ! $scope.mostrar;
+	}
+
 });
