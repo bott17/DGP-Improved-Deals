dfaa79c636607962c451ae50c97d486495ca3be3
JC <alfarol@correo.ugr.es>
2015-05-07 23:35:57 +0200

    Base modificacion datapicker

--- web/js/controllers/homeController.js
+++ web/js/controllers/homeController.js
@@ -1,6 +1,32 @@
 
 angular.module('aloha').controller('homeController', function ($scope, $log) {
 
-   
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+  $scope.dtstart = new Date();
+  $scope.dtend = new Date();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
 
 });
