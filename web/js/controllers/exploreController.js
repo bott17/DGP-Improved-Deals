508bf4af65aade7af643c7566c7b6d3a66e12804
Guillermo Martinez Garcia <jointer@correo.ugr.es>
2015-05-24 11:33:56 +0200

    Cambios Explore

---
+++ web/js/controllers/exploreController.js
@@ -0,0 +1,39 @@
+angular.module('aloha').controller('exploreController', function ($scope, $log, ExploreService) {
+
+  $scope.service = ExploreService;
+
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+  $scope.dtstart;
+  $scope.dtend;
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event,obj) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    if(obj == 'dp1'){
+    	$scope.openeddt1 = true;
+    }else if(obj == 'dp2'){
+    	$scope.openeddt2 = true;
+    }
+
+    console.log(obj);
+  };
+
+});
